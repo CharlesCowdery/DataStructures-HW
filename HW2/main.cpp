f875b86b411743164c9302b3ec330c2457178714
austinheiss <33814164+austinheiss@users.noreply.github.com>
2024-02-13 13:10:21 -0500

    Beginning of main main.cpp

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,1 +1,26 @@
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int moneyCount = 0;
+    cout << "Enter how much money you would like to start with: $";
+    cin >> moneyCount;
+
+    while (true){
+        int wagerModification = 0;
+        cout << "Would you like to modify your wager?\n[1] Double\n[2] Halve\n[3] Keep current wager";
+        cin >> wagerModification;
+
+        while (wagerModification < 1 || wagerModification > 3) {
+            if (wagerModification == 2){
+                //house gets 1 chance
+            } else if (wagerModification == 1 || wagerModification == 3) {
+                //house gets 2 chances
+            } else {
+                cout << "Invalid response, enter only an integer. Would you like to modify your wager?\n[1] Double\n[2] Halve\n[3] Keep current wager";
+                cin >> wagerModification;
+            }
+        }
+
+    }
+}
