dc72f0d891036df63b003e974c06b2d9feaddc39
austinheiss <33814164+austinheiss@users.noreply.github.com>
2024-02-13 13:24:07 -0500

    Update main.cpp

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Game.h"
 using namespace std;
 
 int main() {
@@ -24,3 +25,9 @@ int main() {
 
     }
 }
+
+Wheel playerWheel;
+playerWheel.set_wheel_size(); //enter wherever we get that
+
+Wheel houseWheel;
+houseWheel.set_wheel_size(); //enter wherever we get 
