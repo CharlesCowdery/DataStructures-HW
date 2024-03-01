f791031b9f08c6dc72f81b23abf3a24092fcc91d
Charles Cowdery <charlie.r.cowdery@gmail.com>
2024-03-01 16:00:33 -0500

    main updates

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -9,6 +9,11 @@
 
 using namespace std;
 
+struct resPair {
+	int c1 = 0;
+	int c2 = 0;
+};
+
 class Player {
 protected:
 	side_pile pile;
@@ -16,27 +21,30 @@ protected:
 	string name;
 	int points = 0;
 public:
+	int totCards() { return pile.getlength() + pDeck.getSize(); }
 	void stock_deck(int card) { pDeck.enqueue(card); }
-	virtual int play(Player opp);
+	virtual resPair play(Player opp);
 	virtual void create(int index);
-	virtual int oppPeekDeck();
+	int peekDeck() { return pDeck.getSize(); }
 	string getName() { return name; }
 	
 };
-
 class User : public Player {
 public:
-	int oppPeekDeck() {
-		return pDeck.getSize();
-	}
 	void create(int index) {
 		name = input_s("Enter player " + to_string(index) + " name");
 	}
-	int play(Player opp) {
+	resPair play(Player opp) {
 		printf("%s9   %s9", name.c_str(), opp.getName().c_str());
 		cout << "Deck Pile   Deck Pile" << endl;
-		printf( "%i4 %i4    %i4    ?", pDeck.getSize(), pile.getlength(), opp.oppPeekDeck());
-		int card = pDeck.dequeue();
+		printf( "%i4 %i4    %i4    ?", pDeck.getSize(), pile.getlength(), opp.peekDeck());
+		int card;
+		if (pDeck.getSize() > 0) {
+			card = pDeck.dequeue();
+		}
+		else {
+			card = pile.play();
+		}
 
 		cout << "Drawing" << flush;
 		this_thread::sleep_for(chrono::milliseconds(50)); //dramatic suspense
@@ -48,20 +56,27 @@ public:
 		this_thread::sleep_for(chrono::milliseconds(50));
 		cout << card << endl << endl;
 
-		if (pile.getlength() < 5) cout << "1) Push card\n";
+		bool c1e = pile.getlength() < 5 && pDeck.getSize() > 0;
+		bool c2e = pile.getlength() > 0;
+
+		if (c1e) cout << "1) Push card\n";
 		else cout << "X Push card X\n";
-		if (pile.getlength() > 0) cout << "2) Pull card\n";
+		if (c2e) cout << "2) Pull card\n";
 		else cout << "X Pull card X\n";
 		cout << "3) Play Card" << endl;
 		int choice;
 		while (true) {
 			choice = bounded_input<int>("?: ", "choice", 0b1111, 1, 3);
-			if (choice == 1 && pile.getlength() >= 5) {
-				cout << "!! Deck full, cant push card !!" << endl;
+			if (choice == 1 && !c1e) {
+				if (pDeck.getSize() <= 0) {
+					cout << "!! Deck empty, cant push card !!" << endl;
+				} else if (pile.getlength() >= 5) {
+					cout << "!! Pile full, cant push card !!" << endl;
+				}
 				continue;
 			}
-			if (choice == 2 && pile.getlength() <= 0) {
-				cout << "!! Deck empty, cant pull card !!" << endl;
+			if (choice == 2 && !c2e) {
+				cout << "!! Pile empty, cant pull card !!" << endl;
 				continue;
 			}
 		}
@@ -80,15 +95,15 @@ public:
 			cout << card << endl;
 			
 			cout << "Pulled a " << new_card << endl;
-			return card;
+			return {new_card};
 		}
 		if (choice == 2) {
 			cout << "Card pulled. ";
-			int new_card = pile.play(card);
+			int new_card = pile.play();
 			cout << card << endl;
-			return card + new_card;
+			return { card,new_card };
 		}
-		return card;
+		return { card };
 
 
 	}
@@ -99,17 +114,42 @@ public:
 	void create(int index) {
 		name = "Com" + to_string(index);
 	}
-	int play(Player opp) {
-		int card = pDeck.dequeue();
-		if (card < 5 && pile.getlength() > 0) {
+	resPair play(Player opp) {
+		int card;
+		if (pDeck.getSize() > 0) card = pDeck.dequeue();
+		else card = pile.play();
 
+		if (pDeck.getSize() == 0 && pile.getlength() > 0) {
+			int new_card = pile.play();
+			return { card,new_card };
 		}
+		if (card < 5 ) {
+			if (pile.getlength() > 0) {
+				int new_card = pile.play();
+				return { card,new_card };
+			}
+			return { card };
+		}
+		if (card < 9) {
+			if (pile.getlength() < 5 && pDeck.getSize()>0) {
+				pile.add(card);
+				int new_card = pDeck.dequeue();
+				return { new_card };
+			}
+			return { card };
+		}
+		return { card };
 	}
 };
 
 int main() {
 	Player ply;
 	Player com;
+	int play_type;
+	int num_rounds;
+	int player_wins = 0;
+	bool infinity = false;
+	
 
 	ply = User();
 	ply.create(1);
@@ -117,8 +157,53 @@ int main() {
 	for (int i = 0; i < 52; i++) {
 		ply.stock_deck(i % 13+1);
 	}
+	cout<<"would you like to play until empty(1) or for a number of rounds(2)?"<<endl;
+	cin >> play_type;
+	if(play_type==1){
+		infinity = true;
+		while ((ply.totCards()>0&&com.totCards()>0)) {
+			resPair com_card;
+			resPair ply_card;
+			ply_card = ply.play(com);
+			com_card = com.play(ply);
+			cout << "the computer played a total card value of: " << com_card.c1 + com_card.c2;
+			cout << " You played a card value of : " << ply_card.c1+ply_card.c2 << endl;
+			if ((ply_card.c1+ply_card.c2) > (com_card.c1+com_card.c2)) {
+				cout << "congrats you won!" << endl << "adding cards to your deck!" << endl;
+				ply.stock_deck(ply_card.c1);
+					if (ply_card.c2 != 0) {
+						ply.stock_deck(ply_card.c2);
+					
+					}
+					ply.stock_deck(com_card.c2);
+					if (com_card.c2 != 0) {
+						ply.stock_deck(com_card.c2);
 
-	while (true) {
-		ply.play(com);
+					}
+			
+			}
+			else {
+				cout << "womp womp you lost :(" << endl << "adding cards to computer deck!" << endl;
+				com.stock_deck(ply_card.c1);
+				if (ply_card.c2 != 0) {
+					ply.stock_deck(ply_card.c2);
+
+				}
+				com.stock_deck(com_card.c2);
+				if (com_card.c2 != 0) {
+					com.stock_deck(com_card.c2);
+
+				}
+			
+			
+			}
+		
+		}
+	
+	}
+	else {
+		while (true) {
+			ply.play(com);
+		}
 	}
 }
