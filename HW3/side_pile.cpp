6b501665b9b90aab81766401d0c6eca2922fda3c
austinheiss <33814164+austinheiss@users.noreply.github.com>
2024-03-01 15:53:17 -0500

    added side pile full/empty exception classes

--- HW3/side_pile.cpp
+++ HW3/side_pile.cpp
@@ -1,14 +1,17 @@
 #include "side_pile.h"
+#include "side_pile_empty.h"
+#include "side_pile_full.h"
+
 int side_pile::play() {
 	if (length == 0) {
-		throw "stack underflow";
+		throw StackEmpty();
 	}
 	length--;
 	return pile[length];
 }
 void side_pile::add(int card) {
 	if (length == 5) {
-		throw "stack overflow";
+		throw StackFull();
 	}
 	length++;
 	pile[length] = card;
