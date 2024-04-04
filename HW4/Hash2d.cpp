99dfc435151297b82da81069b2b9091cf7e3ebbf
austinheiss <33814164+austinheiss@users.noreply.github.com>
2024-04-04 19:08:22 -0400

    Initial Hash2d.cpp

---
+++ HW4/Hash2d.cpp
@@ -0,0 +1,73 @@
+#include "Hash2D.h"
+#include <iostream>
+using namespace std;
+
+Hash2d::Hash2d() {
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 5; j++) {
+            nums[i][j] = -1;  // Indicates empty slot
+        }
+    }
+}
+
+Hash2d::~Hash2d() {}
+
+int Hash2d::insert(int value) {
+    int index = hash(value);
+    int spotsChecked = 1;
+
+    for (int i = 0; i < 5; i++) {
+        if (nums[index][i] == -1) {
+            nums[index][i] = value;
+            return spotsChecked;
+        }
+        spotsChecked++;
+    }
+    throw OVERFLOW;
+}
+
+int Hash2d::find(int value) {
+    int index = hash(value);
+    int spotsChecked = 1;
+
+    for (int i = 0; i < 5; i++) {
+        if (nums[index][i] == value) {
+            return spotsChecked;
+        }
+        spotsChecked++;
+    }
+    return spotsChecked;
+}
+
+int Hash2d::remove(int value) {
+    int index = hash(value);
+    int spotsChecked = 1;
+
+    for (int i = 0; i < 5; i++) {
+        if (nums[index][i] == value) {
+            nums[index][i] = -1;
+            return spotsChecked;
+        }
+        spotsChecked++;
+    }
+
+    return spotsChecked;
+}
+
+void Hash2d::print() {
+    for (int i = 0; i < 100; i++) {
+        cout << "Slot " << i << ": ";
+        for (int j = 0; j < 5; j++) {
+            if (nums[i][j] == -1) {
+                cout << "- ";
+            } else {
+                cout << nums[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int Hash2d::hash(int value) {
+    return value % 100;
+}
